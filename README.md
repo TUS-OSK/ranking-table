7a092565d52a2ed2292e3ead1d1ac35a6f6422ea
oginoshikibu <oginoshikibu@gmail.com>
2024-10-17 17:05:46 +0900

    dockerでの開発基盤作成

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # ranking-table
 何かしらの競技結果を保存し、表示するAPI（2024年度理大祭の展示目的）
+
+
+# Docker起動方法
+
+## backend
+
+`~/ranking-table$ docker compose up backend --build`
+
+## db
+
+`~/ranking-table$ docker compose up db --build`
---
+++ src/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello World",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
