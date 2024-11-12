1fa387150479b9e73fdeb989423fbbb432c0fb98
oginoshikibu <oginoshikibu@gmail.com>
2024-11-12 14:30:32 +0900

    見やすく

--- src/models/models.go
+++ src/models/models.go
@@ -29,14 +29,15 @@ func ServiceSeed(db *gorm.DB) {
 
 // test用result
 func ResultSeed(db *gorm.DB) {
-	var results []Result
-	results = append(results, Result{ServiceID: 1, Score: 10, UserName: "user_1"})
-	results = append(results, Result{ServiceID: 1, Score: 20, UserName: "user_2"})
-	results = append(results, Result{ServiceID: 2, Score: 30, UserName: "user_a"})
-	results = append(results, Result{ServiceID: 1, Score: 40, UserName: "user_3"})
-	results = append(results, Result{ServiceID: 1, Score: 40, UserName: "user_4"})
-	results = append(results, Result{ServiceID: 1, Score: 60, UserName: "user_5"})
-	results = append(results, Result{ServiceID: 1, Score: 70, UserName: "user_6"})
+	results := []Result{
+		{ServiceID: 1, Score: 10, UserName: "user_1"},
+		{ServiceID: 1, Score: 20, UserName: "user_2"},
+		{ServiceID: 2, Score: 30, UserName: "user_a"},
+		{ServiceID: 1, Score: 40, UserName: "user_3"},
+		{ServiceID: 1, Score: 40, UserName: "user_4"},
+		{ServiceID: 1, Score: 60, UserName: "user_5"},
+		{ServiceID: 1, Score: 70, UserName: "user_6"},
+	}
 
 	for _, result := range results {
 		db.FirstOrCreate(&result, result)
