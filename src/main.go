c5256d21195f4af79406cfb4bb5c799e1d20c36d
nekoyuki <kaitosaka@icloud.com>
2024-11-15 02:12:42 +0900

    課題2

--- src/main.go
+++ src/main.go
@@ -30,6 +30,28 @@ func main() {
 		c.JSON(http.StatusOK, gin.H{"id": intID})
 	})
 
+	//　練習課題2.1
+	router.POST("/", func(c *gin.Context) {
+		var json struct {
+			Message string "json:\"message\""
+		}
+		c.JSON(http.StatusCreated, gin.H{"message": json.Message})
+	})
+
+	// 練習課題2.2
+	router.GET("/tus/:id", func(c *gin.Context) {
+		id := c.Param("id")
+
+		if id == "1" {
+			c.JSON(http.StatusOK, gin.H{"id": id})
+			return
+		}
+		if id != "1" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+			return
+		}
+	})
+
 	router.Run(":8080")
 
 }
