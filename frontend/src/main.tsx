c2cccfc26d7faba9756165cc0c2054e153b9e982
oginoshikibu <oginoshikibu@gmail.com>
2024-11-20 21:53:21 +0900

    tailwindを導入し

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Leaderboard from './leaderboard'
 import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Leaderboard />
   </StrictMode>,
 )
